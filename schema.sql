891d8eb8d489ab517939e0da5e86604cc0e7a6af
Conor Wantuch <cwantuch7@gmail.com>
2025-04-25 10:53:23 -0600

    added schema's

---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS companies;
+
+CREATE TABLE companies (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    industry VARCHAR(100)
+)
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(25),
+    last_name VARCHAR(25),
+    company_id INTEGER,
+    FOREIGN KEY (company_id) REFERENCES companies(id) ON DELETE CASCADE
+)
